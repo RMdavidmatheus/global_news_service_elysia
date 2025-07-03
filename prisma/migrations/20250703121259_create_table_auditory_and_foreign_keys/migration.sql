9a5967f76ac38b6cbc0c05ab25d19245d9def3e1
davidalejandro.mateus.contractor <davidalejandro.mateus.contractor@bbva.com>
2025-07-03 07:16:18 -0500

    feat: Agregar modelo 'auditory' y relaciones con 'users' y 'task', incluyendo migraciones para la creación y actualización de campos

---
+++ prisma/migrations/20250703121259_create_table_auditory_and_foreign_keys/migration.sql
@@ -0,0 +1,24 @@
+-- CreateTable
+CREATE TABLE "auditory" (
+    "id" TEXT NOT NULL,
+    "id_user" TEXT NOT NULL,
+    "id_task" TEXT NOT NULL,
+    "status" BOOLEAN NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3),
+    "deletedAt" TIMESTAMP(3),
+
+    CONSTRAINT "auditory_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "auditory_id_user_key" ON "auditory"("id_user");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "auditory_id_task_key" ON "auditory"("id_task");
+
+-- AddForeignKey
+ALTER TABLE "auditory" ADD CONSTRAINT "auditory_id_user_fkey" FOREIGN KEY ("id_user") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "auditory" ADD CONSTRAINT "auditory_id_task_fkey" FOREIGN KEY ("id_task") REFERENCES "task"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
